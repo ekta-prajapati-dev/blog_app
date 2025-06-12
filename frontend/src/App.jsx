9dfbf3876fc720f02c07fa161c495c40483aaeb3
Ekta Prajapati <ektaprajapati01@gmail.com>
2025-06-12 06:04:11 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "./index.css";
+import Header from "./components/header";
+
+export default function App() {
+  return <Header></Header>;
+}
